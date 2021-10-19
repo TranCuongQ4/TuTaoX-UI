66266ca3f1e5578277e1bb946a2f73ae7cc7a4cd
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-10-19 21:43:36 +0700

    Update caidatvps.sh

--- caidatvps.sh
+++ caidatvps.sh
@@ -5,7 +5,10 @@ ufw allow 54321
 ufw allow 443
 ufw allow 80
 ufw allow 80/tcp
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt-get install speedtest
 echo " Da Cai Dat Thanh Cong X-UI "  
 echo " Vui Long Vao Trinh Duyet Dan IP VPS Them Duoi :54321 "
 echo " Use admin & Pass admin . Nho Doi Het Thong Tin Tranh Bi Mat "
 echo " Hay Vào Nhom Zalo Chung Toi: https://zalo.me/g/nlsmbo197 "
+speedtest
