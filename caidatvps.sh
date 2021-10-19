c28b7fc84c566a9209e7fdee5a3bf8369fb99944
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-10-19 18:56:21 +0700

    Update caidatvps.sh

--- caidatvps.sh
+++ caidatvps.sh
@@ -5,7 +5,7 @@ ufw allow 54321
 ufw allow 443
 ufw allow 80
 ufw allow 80/tcp
-echo "Da Cai Dat Thanh Cong X-UI .  
-echo "Vui Long Vao Trinh Duyet Dan IP VPS Them Duoi :54321 ."
+echo "Da Cai Dat Thanh Cong X-UI"  
+echo "Vui Long Vao Trinh Duyet Dan IP VPS Them Duoi :54321"
 echo "Use admin & Pass admin . Nho Doi Het Thong Tin Tranh Bi Mat"
 echo "Hay Vào Nhom Zalo Chung Toi: https://zalo.me/g/nlsmbo197"
