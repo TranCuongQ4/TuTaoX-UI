4ce8c5ef225df8f80bf1700a9106d9668a754f7a
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-10-19 21:54:14 +0700

    Update caidatvps.sh

--- caidatvps.sh
+++ caidatvps.sh
@@ -7,8 +7,9 @@ ufw allow 80
 ufw allow 80/tcp
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest
-echo " Da Cai Dat Thanh Cong X-UI "  
-echo " Vui Long Vao Trinh Duyet Dan IP VPS Them Duoi :54321 "
-echo " Use admin & Pass admin . Nho Doi Het Thong Tin Tranh Bi Mat "
-echo " Hay Vào Nhom Zalo Chung Toi: https://zalo.me/g/nlsmbo197 "
+echo " Đã Cài Đặt thành Công X-UI "  
+echo " Vui Lòng Vào Trình Duyệt Nhập IP VPS Và Thêm Vào Đuôi Là :54321 "
+echo " Use admin & Pass admin . Nhớ Đổi Hết Thông Tin Tránh Bị Mất "
+echo " Vào Nhóm Zalo Chúng Tôi Cùng Giao Lưu Nhé : https://zalo.me/g/nlsmbo197 "
 speedtest
+echo " Bây Giờ Bạn vào X-UI Sửa Thông Tin Và Tạo Vmess Trojan Vless Đợi Gì Nữa Nào "
