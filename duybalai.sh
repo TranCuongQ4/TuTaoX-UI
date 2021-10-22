9f18aa628041a286974caeb974651323ddce3fc7
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-10-22 15:23:22 +0700

    Create duybalai.sh

---
+++ duybalai.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+apt-get update -y
+bash <(curl -Ls https://raw.githubusercontent.com/TranCuongQ4/X-UI-VH/master/install.sh)
+ufw allow 54321
+ufw allow 443
+ufw allow 80
+ufw allow 80/tcp
+echo " Đã Cài Đặt Thành Công X-UI & SpeedTest "  
+echo " Vui Lòng Vào Trình Duyệt Nhập IP VPS Và Thêm Vào Đuôi Là :54321 "
+echo " Use admin & Pass admin . Nhớ Đổi Hết Thông Tin Tránh Bị Mất "
+echo " Nhớ Đổi 54321 Thành 5xxxx Giữ Lại 5 Đầu Nhé Không Bạn Bị Người Khác Đổi Đấy "
+echo " Vào Nhóm Zalo Chúng Tôi Cùng Giao Lưu Nhé : https://zalo.me/g/nlsmbo197 "
+echo " Bây Giờ Bạn Vào X-UI Sửa Thông Tin Và Tạo Vmess Trojan Vless Đợi Gì Nữa Nào "
+echo " Liên Hệ Với Minh Duy Qua Điện Thoại & Zalo Để Có Giá VPS Hợp Lý Nhé : 0766884380 "
+echo " Nếu Muốn Kiểm Tra Tốc Độ VPS Thì Nhấn Lệnh (speedtest) Không Dấu Ngoặc Nha "
+echo " Giữ Sức Khoẻ Khi Sử Dụng Internet ... Hoàn Tất ... "
+echo " Hihihihi *-* *-* Hehehehe "
+echo "   ───▄▄▄▄▄▄─────▄▄▄▄▄▄  "
+echo "   ─▄█▓▓▓▓▓▓█▄─▄█▓▓▓▓▓▓█▄  "
+echo "   ▐█▓▓▒▒▒▒▒▓▓█▓▓▒▒▒▒▒▓▓█▌  "
+echo "   █▓▓▒▒░╔╗╔═╦═╦═╦═╗░▒▒▓▓█  "
+echo "   █▓▓▒▒░║╠╣╬╠╗║╔╣╩╣░▒▒▓▓█  "
+echo "   ▐█▓▓▒▒╚═╩═╝╚═╝╚═╝▒▒▓▓█▌  "
+echo "   ─▀█▓▓▒▒░░░░░░░░░▒▒▓▓█▀  "
+echo "   ───▀█▓▓▒▒░░░░░▒▒▓▓█▀  "
+echo "   ─────▀█▓▓▒▒░▒▒▓▓█▀  "
+echo "    ──────▀█▓▓▒▓▓█▀  "
+echo "    ────────▀█▓█▀  "
+echo "    ──────────▀  "
+echo " Hoàn Tất "
