d07463437dcb9e3589ffece939b1d66a6a70bc54
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-11-01 12:06:46 +0700

    Update menutrancuong.sh

--- menutrancuong.sh
+++ menutrancuong.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
+rm menucaidat.sh
+rm menucaidat.sh.1
+rm menucaidat.sh.2
+rm menucaidat.sh.3
+rm menucaidat.sh.4
+rm menucaidat.sh.5
+rm menucaidat.sh.6
+rm menucaidat.sh.7
+rm menucaidat.sh.8
+rm menucaidat.sh.9
 clear
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install bash
 wget https://raw.githubusercontent.com/TranCuongQ4/X-UI-VH/master/menucaidat.sh
 chmod +x menucaidat.sh
-./menucaidat.sh
+bash menucaidat.sh
